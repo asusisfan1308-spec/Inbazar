c804d6ba92f3e1bdf7afcfa4aa520272f1ea665d
Isfandiyor <isfan13082010@example.com>
2026-08-31 21:41:19 +0500

    Final

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', include('page.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
